ef5838e518be6ccad0a99acd792f93ea6cebaa57
dcontt00 <dcontt00@estudiantes.unileon.es>
2024-01-08 21:03:46 +0100

    Implementado APA generator

--- entrypoints/apa-generator.content/index.ts
+++ entrypoints/apa-generator.content/index.ts
@@ -9,31 +9,89 @@ export default defineContentScript({
     browser.runtime.onMessage.addListener((message) => {
       console.log("message", message);
       if (message.command === "apa") {
-        var citation;
-        let title = document.getElementById(
+        var citation: string;
+        var title: string;
+        var authors: string;
+        var date: string;
+        var journal: string;
+        var volume: string;
+        var doi: string = "";
+
+        let titleElement = document.getElementById(
           "aspect_submission_StepTransformer_field_dc_title"
         ) as HTMLInputElement;
 
-        let alttitle = document.getElementById(
-          "aspect_submission_StepTransformer_field_dc_title_alternative"
-        ) as HTMLInputElement;
-
-        alttitle && (alttitle.value = title.value);
-
-        var authors: string[] = [];
         let authorsCheckboxes = document.querySelectorAll(
           "[name='dc_contributor_author_selected']"
         );
-
+        var authorsArray: string[] = [];
         authorsCheckboxes.forEach((checkbox) => {
           var parent = checkbox.parentElement;
-          var author: HTMLCollectionOf<HTMLSpanElement> | undefined =
+          var authorElement: HTMLCollectionOf<HTMLSpanElement> | undefined =
             parent?.getElementsByTagName("span");
-          author && authors.push(author[0].innerText);
+          authorElement && authorsArray.push(authorElement[0].innerText);
         });
 
-        alert("title: " + title.value + "\nauthors: " + authors.join("; "));
+        let dateElement: HTMLInputElement = document.getElementById(
+          "aspect_submission_StepTransformer_field_dc_date_year"
+        ) as HTMLInputElement;
+
+        let journalElement: HTMLInputElement = document.getElementById(
+          "aspect_submission_StepTransformer_field_dc_journal_title"
+        ) as HTMLInputElement;
+
+        let volumeElement: HTMLInputElement = document.getElementById(
+          "aspect_submission_StepTransformer_field_dc_volume_number"
+        ) as HTMLInputElement;
+
+        let doiCheckboxes = document.querySelectorAll(
+          "[name='dc_identifier_selected']"
+        );
+        doiCheckboxes.forEach((checkbox) => {
+          var parent = checkbox.parentElement;
+          var doiElement: HTMLCollectionOf<HTMLSpanElement> | undefined =
+            parent?.getElementsByTagName("span");
+
+          if (doiElement && doiElement[0].innerText.includes("doi:")) {
+            doi = doiElement[0].innerText.replace("doi:", "");
+          }
+        });
+
+        let bibliographyElement: HTMLInputElement = document.getElementById(
+          "aspect_submission_StepTransformer_field_dc_identifier_citation"
+        ) as HTMLInputElement;
+
+        title = titleElement?.value;
+        authors = formatAuthors(authorsArray);
+        date = dateElement?.value;
+        journal = journalElement?.value;
+        volume = volumeElement?.value;
+
+        citation = `${authors} (${date}). ${title}. ${journal}, ${volume}, ${doi}`;
+        bibliographyElement.value = citation;
       }
     });
   },
 });
+
+/**
+ * Formats an array of authors into a specific format.
+ *
+ * @param authors - The array of authors to be formatted.
+ * @returns The formatted authors as a string.
+ */
+function formatAuthors(authors: string[]) {
+  var formattedAuthors: string[] = [];
+  authors.forEach((author) => {
+    var authorParts = author.split(", ");
+    var surnames = authorParts[0].toUpperCase();
+    var names = authorParts[1].toUpperCase().split(" ");
+    var formattedName = "";
+    names.forEach((name) => {
+      formattedName += `${name[0]}. `;
+    });
+    var formattedAuthor = `${surnames}, ${formattedName}`;
+    formattedAuthors.push(formattedAuthor);
+  });
+  return formattedAuthors.join("; ");
+}
