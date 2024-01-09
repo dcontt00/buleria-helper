15502f2aa6a940bd368f9f9d5084e5349d9a8501
dcontt00 <dcontt00@estudiantes.unileon.es>
2024-01-09 21:44:01 +0100

    Añadido tema

---
+++ theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+var theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#00bcd4",
+    },
+    secondary: {
+      main: "#ff4081",
+    },
+  },
+});
+
+export default theme;
