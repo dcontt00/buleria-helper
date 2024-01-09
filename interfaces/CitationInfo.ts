bf24ea59803dabc002f958541c127a37fc47d935
dcontt00 <dcontt00@estudiantes.unileon.es>
2024-01-09 13:29:06 +0100

    Creada interfa CitationInfo

---
+++ interfaces/CitationInfo.ts
@@ -0,0 +1,8 @@
+export default interface CitationInfo {
+  title: string | undefined;
+  authors: string | undefined;
+  date: string | undefined;
+  journal: string | undefined;
+  volume: string | undefined;
+  doi: string | undefined;
+}
