8ef3bbcfa18928e70a12da92b4ab137f7b56fdaf
dcontt00 <dcontt00@estudiantes.unileon.es>
2024-01-09 11:27:27 +0100

    Movido ApaGenerator a component

---
+++ components/ApaGenerator.tsx
@@ -0,0 +1,21 @@
+import { browser } from "wxt/browser"
+
+export default function ApaGenerator() {
+    async function onClick() {
+        console.log("Clicking button");
+        var tab = (await browser.tabs.query({ active: true, currentWindow: true })).pop();
+        try {
+            if (tab) {
+                await browser.tabs.sendMessage(tab.id || 0, { command: "apa" });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+    return (
+        <div className="card">
+            <button id='myButton' onClick={onClick}>Generar APA </button>
+        </div>
+    )
+}
--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -2,21 +2,9 @@ import reactLogo from '@/assets/react.svg';
 import wxtLogo from '/wxt.svg';
 import { browser } from "wxt/browser"
 import './App.css';
+import ApaGenerator from '@/components/ApaGenerator';
 
 function App() {
-
-  async function onClick() {
-    console.log("Clicking button");
-    var tab = (await browser.tabs.query({ active: true, currentWindow: true })).pop();
-    try {
-      if (tab) {
-        await browser.tabs.sendMessage(tab.id || 0, { command: "apa" });
-      }
-    } catch (error) {
-      console.log(error);
-    }
-
-  }
 
   return (
     <>
@@ -24,9 +12,7 @@ function App() {
         <img src={wxtLogo} className="logo" alt="WXT logo" />
       </div>
       <h1>WXT + React</h1>
-      <div className="card">
-        <button id='myButton' onClick={onClick}>Generar APA </button>
-      </div>
+      <ApaGenerator />
     </>
   );
 }
