8693b318e90cd9cef97d07b88d33e19f139d35c5
dcontt00 <dcontt00@estudiantes.unileon.es>
2024-01-12 12:50:52 +0100

    Añadidos iconos

--- components/SherpaRomeo/PublisherPolicyData.tsx
+++ components/SherpaRomeo/PublisherPolicyData.tsx
@@ -1,16 +1,24 @@
 import { PublisherPolicy } from "@/types"
-import { Grid, Link, Paper, Stack, Typography } from "@mui/material"
-
+import { Box, Grid, Link, Paper, Stack, Typography } from "@mui/material"
+import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
+import FolderIcon from '@mui/icons-material/Folder';
+import ChecklistIcon from '@mui/icons-material/Checklist';
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import CopyrightIcon from '@mui/icons-material/Copyright';
+import BackupIcon from '@mui/icons-material/Backup';
 export default function SherpaRomeo({ PublisherPolicies }: { PublisherPolicies: PublisherPolicy[] }) {
 
     function Conditions(PublisherPolicy: PublisherPolicy) {
         if (PublisherPolicy.conditions != undefined && PublisherPolicy.conditions.length !== 0) {
             return (
                 <>
-                    <Grid item xs={4}>
-                        <Typography variant="body1">Condiciones</Typography>
+                    <Grid item xs={5}>
+                        <Stack direction="row" spacing={1}>
+                            <ChecklistIcon />
+                            <Typography variant="body1" align="left">Condiciones</Typography>
+                        </Stack>
                     </Grid>
-                    <Grid item xs={8}>
+                    <Grid item xs={7}>
                         {PublisherPolicy.conditions.map((condition, index) => {
                             return (
                                 <Typography variant="body1" key={index} align="left">{condition}</Typography>
@@ -28,10 +36,13 @@ export default function SherpaRomeo({ PublisherPolicies }: { PublisherPolicies: 
         if (PublisherPolicy.locations != undefined && PublisherPolicy.locations.length !== 0) {
             return (
                 <>
-                    <Grid item xs={4}>
-                        <Typography variant="body1">Localizacion</Typography>
+                    <Grid item xs={5}>
+                        <Stack direction="row" spacing={1}>
+                            <FolderIcon />
+                            <Typography variant="body1" align="left">Localizacion</Typography>
+                        </Stack>
                     </Grid>
-                    <Grid item xs={8}>
+                    <Grid item xs={7}>
                         {PublisherPolicy.locations.map((location, index) => {
                             return (
                                 <Typography variant="body1" key={index} align="left">{location}</Typography>
@@ -49,10 +60,13 @@ export default function SherpaRomeo({ PublisherPolicies }: { PublisherPolicies: 
         if (PublisherPolicy.license != undefined) {
             return (
                 <>
-                    <Grid item xs={4}>
-                        <Typography variant="body1">Licencia</Typography>
+                    <Grid item xs={5}>
+                        <Stack direction="row" spacing={1}>
+                            <ReceiptLongIcon />
+                            <Typography variant="body1" align="left">Licencia</Typography>
+                        </Stack>
                     </Grid>
-                    <Grid item xs={8}>
+                    <Grid item xs={7}>
                         <Typography variant="body1" align="left">{PublisherPolicy.license}</Typography>
                     </Grid>
                 </>
@@ -64,10 +78,13 @@ export default function SherpaRomeo({ PublisherPolicies }: { PublisherPolicies: 
         if (PublisherPolicy.embargo != undefined) {
             return (
                 <>
-                    <Grid item xs={4}>
-                        <Typography variant="body1">Embargo</Typography>
+                    <Grid item xs={5}>
+                        <Stack direction="row" spacing={1}>
+                            <HourglassBottomIcon />
+                            <Typography variant="body1" align="left">Embargo</Typography>
+                        </Stack>
                     </Grid>
-                    <Grid item xs={8}>
+                    <Grid item xs={7}>
                         <Typography variant="body1" align="left">{PublisherPolicy.embargo} </Typography>
                     </Grid>
                 </>
@@ -78,10 +95,13 @@ export default function SherpaRomeo({ PublisherPolicies }: { PublisherPolicies: 
         if (PublisherPolicy.copyrightOwner != undefined) {
             return (
                 <>
-                    <Grid item xs={4}>
-                        <Typography variant="body1">Copyright Owner</Typography>
+                    <Grid item xs={5}>
+                        <Stack direction="row" spacing={1}>
+                            <CopyrightIcon />
+                            <Typography variant="body1" align="left">Copyright Owner</Typography>
+                        </Stack>
                     </Grid>
-                    <Grid item xs={8}>
+                    <Grid item xs={7}>
                         <Typography variant="body1" align="left">{PublisherPolicy.copyrightOwner} </Typography>
                     </Grid>
                 </>
@@ -92,10 +112,13 @@ export default function SherpaRomeo({ PublisherPolicies }: { PublisherPolicies: 
         if (PublisherPolicy.publisherDeposit != undefined) {
             return (
                 <>
-                    <Grid item xs={4}>
-                        <Typography variant="body1">Publisher Deposit</Typography>
+                    <Grid item xs={5}>
+                        <Stack direction="row" spacing={1}>
+                            <BackupIcon />
+                            <Typography variant="body1" align="left">Publisher Deposit</Typography>
+                        </Stack>
                     </Grid>
-                    <Grid item xs={8}>
+                    <Grid item xs={7}>
                         <Link href={PublisherPolicy.publisherDeposit.url} target="_blank" rel="noopener noreferrer">
                             <Typography variant="body1" align="left">{PublisherPolicy.publisherDeposit.name} </Typography>
                         </Link>
