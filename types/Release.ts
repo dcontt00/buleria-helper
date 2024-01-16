82b017f47a796b6fc4bd41e75795f4e787db5867
dcontt00 <dcontt00@estudiantes.unileon.es>
2024-01-16 13:22:26 +0100

    Añadido componente Updater

---
+++ types/Release.ts
@@ -0,0 +1,8 @@
+type Release = {
+  id: number;
+  name: string;
+  tag_name: string;
+  body: string;
+  url: string;
+  download_url: string;
+};
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,5 @@
 import PublisherPolicy from "./PublisherPolicy";
 import Author from "./Author";
+import Release from "./Release";
 
-export { PublisherPolicy, Author };
+export { PublisherPolicy, Author, Release };
