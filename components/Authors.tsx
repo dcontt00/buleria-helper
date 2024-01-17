e2b98bfc48c60e26ffc29c3f38fef55781f99a8e
dcontt00 <dcontt00@estudiantes.unileon.es>
2024-01-17 11:28:27 +0100

    Añadido Progress

--- components/Authors.tsx
+++ components/Authors.tsx
@@ -5,11 +5,15 @@ import { Author } from "@/types";
 import { sendMessage } from "@/utils/messaging";
 import ComponentProps from "@/interfaces/ComponentProps";
 import waitForTabComplete from "@/utils/tabUtils";
+import ProgressComponent from "./Progress";
 
 export default function Authors({ tab }: ComponentProps) {
     const [authors, setAuthors] = useState<Author[]>([]);
     const [name, setName] = useState<string>("");
     const [surname, setSurname] = useState<string>("");
+    const [progress, setProgress] = useState<number>(0);
+    const [showProgress, setShowProgress] = useState<boolean>(false);
+    const [buttonDisabled, setButtonDisabled] = useState<boolean>(true);
 
     function onChangeName(event: React.ChangeEvent<HTMLInputElement>) {
         setName(event.target.value);
@@ -22,22 +26,26 @@ export default function Authors({ tab }: ComponentProps) {
         setAuthors([...authors, { name: name, surname: surname }]);
         setName("");
         setSurname("");
+        setButtonDisabled(false);
     }
     function onDelete(index: number) {
         setAuthors(authors.filter((_, i) => i !== index));
     }
 
     async function onAddAuthorsClick() {
-        console.log(authors);
-        if (tab) {
-            for (let author of authors) {
-                var response = await sendMessage("pasteAuthor", author, tab.id);
-                console.log(response);
-                // Esperar a que la pagina termine de cargar
-                await waitForTabComplete(tab?.id);
-            }
-
+        setButtonDisabled(true);
+        setProgress(0);
+        var increment = 100 / authors.length;
+        setShowProgress(true);
+        for (let author of authors) {
+            var response = await sendMessage("pasteAuthor", author, tab.id);
+            console.log(response);
+            // Esperar a que la pagina termine de cargar
+            await waitForTabComplete(tab?.id);
+            setProgress(oldProgress => oldProgress + increment);
         }
+        setShowProgress(false);
+        setButtonDisabled(false);
     }
     return (
         <div>
@@ -54,7 +62,8 @@ export default function Authors({ tab }: ComponentProps) {
                 }
                 )}
             </Stack>
-            <Button variant="contained" onClick={onAddAuthorsClick}>Añadir autores</Button>
+            <Button variant="contained" onClick={onAddAuthorsClick} disabled={buttonDisabled}>Añadir autores</Button>
+            <ProgressComponent progress={progress} showProgress={showProgress} progressText="Añadiendo autores" completeText="Añadidos autores" />
         </div>
     )
 
