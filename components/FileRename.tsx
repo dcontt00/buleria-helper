844010460b27f5cd89dd900ff56a639829be220f
dcontt00 <dcontt00@estudiantes.unileon.es>
2024-01-19 11:56:52 +0100

    Movida funcion

--- components/FileRename.tsx
+++ components/FileRename.tsx
@@ -2,42 +2,15 @@ import { Button, Stack, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import HideAlert from "./HideAlert";
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-
+import { getFileNameFromTitle } from "@/utils/stringUtils";
 export default function FileRename() {
     const [title, setTitle] = useState<string>("");
     const [filename, setFilename] = useState<string>("");
 
-    /**
-     * Transforms a string.
-     * 
-     * @param input The input string to transform.
-     * @returns The transformed string.
-     */
-    function transformString(input: string): string {
-
-        // Eliminar espacio en blanco inicial si hay
-        input = input.trim();
-
-        // Eliminar caracteres especiales
-        input = input.replace(/-/g, ' ');
-
-        // Eliminar simbolos puntuacion
-        const punctuationRegex = /[.,;?¿¿!(){}\\[\\]-]/g;
-        const withoutPunctuation = input.replace(punctuationRegex, '');
-        console.log(withoutPunctuation);
-
-        // Eliminar preposiciones, conjunciones y articulos
-        const prepositionsAndConjunctions = ["de", "a", "en", "y", "o", "con", "por", "para", "sin", "sobre", "bajo", "entre", "hacia", "desde", "hasta", "según", "durante", "mediante", "versus", "vía", "el", "la", "los", "las", "un", "una", "unos", "unas", "of", "to", "in", "and", "or", "with", "by", "for", "without", "on", "under", "between", "towards", "from", "until", "according", "during", "through", "versus", "via", "the", "a", "an"];
-        const words = withoutPunctuation.split(' ').filter(word => !prepositionsAndConjunctions.includes(word.toLowerCase()));
-
-        // Coger las 5 primeras palabras y poner mayuscula la primera letra de cada palabra
-        const transformedWords = words.slice(0, 5).map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
-        return transformedWords.join('_');
-    }
 
     function onChangeTitle(event: React.ChangeEvent<HTMLInputElement>) {
         setTitle(event.target.value);
-        const transformedString = transformString(event.target.value);
+        const transformedString = getFileNameFromTitle(event.target.value);
         setFilename(transformedString);
     }
 
--- utils/stringUtils.ts
+++ utils/stringUtils.ts
@@ -1,5 +1,86 @@
-export default function capitalizeWords(word: string) {
+function capitalizeWords(word: string) {
   return word.replace(/\b\w/g, function (char) {
     return char.toUpperCase();
   });
 }
+
+function getFileNameFromTitle(title: string) {
+  // Eliminar espacio en blanco inicial si hay
+  title = title.trim();
+
+  // Eliminar caracteres especiales
+  title = title.replace(/-/g, " ");
+
+  // Eliminar simbolos puntuacion
+  const punctuationRegex = /[.,;?¿¿!(){}\\[\\]-]/g;
+  const withoutPunctuation = title.replace(punctuationRegex, "");
+  console.log(withoutPunctuation);
+
+  // Eliminar preposiciones, conjunciones y articulos
+  const prepositionsAndConjunctions = [
+    "de",
+    "a",
+    "en",
+    "y",
+    "o",
+    "con",
+    "por",
+    "para",
+    "sin",
+    "sobre",
+    "bajo",
+    "entre",
+    "hacia",
+    "desde",
+    "hasta",
+    "según",
+    "durante",
+    "mediante",
+    "versus",
+    "vía",
+    "el",
+    "la",
+    "los",
+    "las",
+    "un",
+    "una",
+    "unos",
+    "unas",
+    "of",
+    "to",
+    "in",
+    "and",
+    "or",
+    "with",
+    "by",
+    "for",
+    "without",
+    "on",
+    "under",
+    "between",
+    "towards",
+    "from",
+    "until",
+    "according",
+    "during",
+    "through",
+    "versus",
+    "via",
+    "the",
+    "a",
+    "an",
+  ];
+  const words = withoutPunctuation
+    .split(" ")
+    .filter(
+      (word) => !prepositionsAndConjunctions.includes(word.toLowerCase())
+    );
+
+  // Coger las 5 primeras palabras y poner mayuscula la primera letra de cada palabra
+  const transformedWords = words
+    .slice(0, 5)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+  return transformedWords.join("_");
+}
+
+export { capitalizeWords, getFileNameFromTitle };
