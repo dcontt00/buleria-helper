83bdb981c4cdc2ad7a31f4aff0fd3d4fb89fa1c8
dcontt00 <dcontt00@estudiantes.unileon.es>
2024-01-19 12:46:51 +0100

    al escribir se actualiza el nombre de archivo

--- entrypoints/file-rename.content/TitleTextfield.tsx
+++ entrypoints/file-rename.content/TitleTextfield.tsx
@@ -3,14 +3,32 @@ import { Button, IconButton, Paper, Stack, TextField, Tooltip } from "@mui/mater
 import { ThemeProvider } from "@mui/material"
 import theme from "@/utils/theme"
 import FileCopyIcon from '@mui/icons-material/FileCopy';
+import { useState, useEffect } from "react";
+
 interface TitleTextfieldProps {
-    title: string
+    title: string;
 }
 
 export default function TitleTextfield({ title }: TitleTextfieldProps) {
+    const [value, setValue] = useState<string>(getFileNameFromTitle(title));
+    useEffect(() => {
+        let titleElement = document.getElementById(
+            "aspect_submission_StepTransformer_field_dc_title"
+        ) as HTMLInputElement;
+
+        const handleInputChange = () => {
+            setValue(getFileNameFromTitle(titleElement.value));
+        };
+
+        titleElement.addEventListener('input', handleInputChange);
+
+        return () => {
+            titleElement.removeEventListener('input', handleInputChange);
+        };
+    }, []);
     const handleCopy = async () => {
         try {
-            await navigator.clipboard.writeText(getFileNameFromTitle(title));
+            await navigator.clipboard.writeText(value);
             console.log('Text copied to clipboard');
         } catch (err) {
             console.error('Failed to copy text: ', err);
@@ -23,7 +41,7 @@ export default function TitleTextfield({ title }: TitleTextfieldProps) {
                 <TextField id="title" label="Título de archivo" variant="outlined" fullWidth
                     InputLabelProps={{ style: { fontSize: 16 } }}
                     InputProps={{ style: { fontSize: 16 } }}
-                    value={getFileNameFromTitle(title)}
+                    value={value}
                 />
                 <Button variant="contained" onClick={handleCopy} startIcon={<FileCopyIcon />} >Copiar</Button>
             </Stack>
--- entrypoints/keywords.content/index.ts
+++ entrypoints/keywords.content/index.ts
@@ -1,49 +1,15 @@
 import { defineContentScript } from "wxt/sandbox";
 import { onMessage } from "@/utils/messaging";
-
+import { getKeywords, pasteKeyword } from "./common";
 export default defineContentScript({
   matches: ["https://buleria.unileon.es/*"],
   runAt: "document_end",
   main: () => {
     onMessage("pasteKeyword", (keyword) => {
-      const input: HTMLInputElement | null = document.getElementById(
-        "aspect_submission_StepTransformer_field_dc_subject_other"
-      ) as HTMLInputElement;
-      const button: HTMLButtonElement | null = document.querySelector(
-        'button[name="submit_dc_subject_other_add"]'
-      );
-      console.log("pasteKeywords");
-
-      // Asegúrate de que el input y el botón existen
-      if (input && button) {
-        // Define los textos que quieres añadir
-
-        // Añade cada texto, uno por uno
-        try {
-          // Añade el texto al input
-          input.value = keyword.data;
-
-          // Haz clic en el botón
-          button.click();
-        } catch (error) {
-          return false;
-        }
-      }
-      return true;
+      return pasteKeyword(keyword.data);
     });
     onMessage("getKeywords", (message) => {
-      const inputElements: NodeListOf<HTMLInputElement> =
-        document.querySelectorAll('input[name="dc_subject_other_selected"]');
-
-      var keywords = inputElements[0].parentElement?.textContent;
-      const removeButton: HTMLButtonElement | null = document.querySelector(
-        "button[name='submit_dc_subject_other_delete']"
-      );
-      inputElements[0].click();
-      removeButton?.click();
-
-      // Return as string
-      return keywords;
+      return getKeywords();
     });
   },
 });
