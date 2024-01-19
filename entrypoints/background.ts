9033147fc646af8aab40ecbff72d61a00c6d1598
dcontt00 <dcontt00@estudiantes.unileon.es>
2024-01-19 12:48:47 +0100

    Quitados console.logs

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -3,11 +3,9 @@ import waitForTabComplete from "@/utils/tabUtils";
 export default defineBackground(() => {
   browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete" && tab.active) {
-      console.log("Tab updated", tabId, changeInfo, tab);
       await waitForTabComplete(tabId);
-      var res = await sendMessage("addCitationButtonToPage", undefined, tabId);
+      await sendMessage("addCitationButtonToPage", undefined, tabId);
       await sendMessage("addTitleButtonToPage", undefined, tabId);
-      console.log("addCitationButtonToPage result", res);
     }
   });
 });
