4c70b3e346230b17eeb4884170ae2b231af1c5c1
dcontt00 <dcontt00@estudiantes.unileon.es>
2024-01-24 14:01:11 +0100

    Creadas clases

--- entrypoints/apa-generator.content/BookChapter.ts
+++ entrypoints/apa-generator.content/BookChapter.ts
@@ -1,29 +1,31 @@
 import { formatAuthors } from "./common";
 
-class Book {
+class BookChapter {
   authors: string[];
   date: string;
   title: string;
+  bookTitle: string;
   editorial: string;
 
   constructor(
     authors: string[],
     date: string,
     title: string,
+    bookTitle: string,
     editorial: string
   ) {
     this.authors = authors;
     this.date = date;
     this.title = title;
+    this.bookTitle = bookTitle;
     this.editorial = editorial;
   }
-
-  // Autores. (Fecha). Titulo. Editorial
+  // Autores. (Fecha). Titulo Capitulo. En Titulo libro. (p. paginas) Editorial
   toString(): string {
-    return `${formatAuthors(this.authors)} (${this.date}). ${this.title}. ${
-      this.editorial
-    }`;
+    return `${formatAuthors(this.authors)} (${this.date}). ${this.title}. En ${
+      this.bookTitle
+    }. ${this.editorial}`;
   }
 }
 
-export default Book;
+export default BookChapter;
