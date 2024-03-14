817ab040fb2f2e8d663bd3f15b34234f3f84e4e2
dcontt00 <dcontt00@estudiantes.unileon.es>
2024-03-14 12:50:22 +0100

    Version 1.3.2

--- components/SherpaRomeo/index.tsx
+++ components/SherpaRomeo/index.tsx
@@ -35,6 +35,7 @@ export default function SherpaRomeo({ tab }: ComponentProps) {
                 setIssn(issn);
                 return;
             }
+            setNotFound(true);
         });
     }
 
