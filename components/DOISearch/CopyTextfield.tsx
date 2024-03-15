0eee6ff8729da1d38f841ca5488a6e1f6e466818
dcontt00 <dcontt00@estudiantes.unileon.es>
2024-03-15 12:27:48 +0100

    Creado copytextfield

---
+++ components/DOISearch/CopyTextfield.tsx
@@ -0,0 +1,35 @@
+import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+import { IconButton, InputAdornment, TextField } from "@mui/material";
+
+interface CopyTextFieldProps {
+    label: string;
+    text: string;
+    fullWidth?: boolean;
+    multiline?: boolean;
+}
+
+export default function CopyTextField({ label, text, fullWidth, multiline }: CopyTextFieldProps) {
+    async function onClickCopy() {
+        await navigator.clipboard.writeText(text);
+
+    }
+    return (
+        <TextField
+            id="outlined-basic"
+            label={label}
+            variant="outlined"
+            value={text}
+            fullWidth={fullWidth}
+            multiline={multiline}
+            InputProps={{
+                readOnly: true, endAdornment: (
+                    <InputAdornment position="end">
+                        <IconButton onClick={onClickCopy}>
+                            <ContentPasteIcon />
+                        </IconButton>
+                    </InputAdornment>
+                ),
+            }}
+        />
+    )
+}
